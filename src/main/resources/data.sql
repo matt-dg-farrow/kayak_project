66b1ba1c0dde427eab395ec5f56322c9c8573ea2
Matthew Farrow <matt.dg.farrow@gmail.com>
2020-01-10 16:27:18 +0000

    Added equipment to be available at launch

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,40 @@ INSERT INTO equipment VALUES
 (default, 'kayak', 60, null), (default, 'kayak', 60, null),
 (default, 'kayak', 60, null), (default, 'kayak', 60, null),
 (default, 'kayak', 60, null), (default, 'kayak', 60, null),
-(default, 'kayak', 60, null), (default, 'kayak', 60, null);
+(default, 'kayak', 60, null), (default, 'kayak', 60, null),
+
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+(default, 'helmet', 15, null), (default, 'helmet', 15, null),
+
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+(default, 'BA', 30, null), (default, 'BA', 30, null),
+
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 60, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null),
+(default, 'paddle', 20, null), (default, 'paddle', 20, null);
 
 
 
